c029c2ae1823f5ee4f5575ff753e44e50724dc6b
Javier Cordero <jcorderomartinez@gmail.com>
2014-08-04 13:00:47 +0200

    python3 compatibility

--- mailfactory_extras/utils.py
+++ mailfactory_extras/utils.py
@@ -4,9 +4,15 @@
 
 
 from mail_factory import factory
-
+import six
 
 def registered_mails_names():
-    for k, v in factory._registry.items():
-        yield k, v.__name__
-    return
+    if hasattr(factory, '_registry'):
+        for k, v in six.iteritems(factory._registry):
+            yield k, v.__name__
+        return
+    else:
+        for k, v in six.iteritems(factory.mail_map):
+            yield k, v.__name__
+        return
+
